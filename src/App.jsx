93496d8e7565c0dbc1e966c643bf67da4baef03d
davit.chanturia <chanturiadato@gmail.com>
2024-04-23 20:58:35 +0400

    chore: installing material ui component library

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">
         Hello world!
       </h1>
+      <Button variant="contained">Hello world</Button>;
     </>
   )
 }
