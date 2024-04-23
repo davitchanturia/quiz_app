00655a46c3b04a26ea8ce269eaf4dae09fed34b7
davit.chanturia <chanturiadato@gmail.com>
2024-04-23 23:00:14 +0400

    chore: installing prettier in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import Button from '@mui/material/Button';
-
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
-      <Button variant="contained">Hello world</Button>;
+      <h1 className="text-3xl font-bold">Hello world!</h1>
     </>
   )
 }
