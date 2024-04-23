70fd61c0d1d96941da5abdeac353111da38fda81
davit.chanturia <chanturiadato@gmail.com>
2024-04-23 20:52:12 +0400

    chore: installing axios and creating base services

---
+++ src/services/QuizApi.js
@@ -0,0 +1,13 @@
+import { apiClient } from "../helpers/apiClient"
+
+export const getAllCategories = async() => {
+    return await apiClient.get('categories');
+}
+
+export const getAllTags = async() => {
+    return await apiClient.get('tags');
+}
+
+export const getQuestions = async(filterParameters = {}) => {
+    return await apiClient.get('questions', filterParameters);
+}
